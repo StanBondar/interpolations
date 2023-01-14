58376163ecb47ed1764dcac821be5961bfb6da4b
Stan Bondar <bondar.stan@gmail.com>
2023-01-14 22:49:13 +0200

    added input chage handlers

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cp1X = document.getElementById("controlPoint1X");
 const cp1Y = document.getElementById("controlPoint1Y");
 const cp2X = document.getElementById("controlPoint2X");
 const cp2Y = document.getElementById("controlPoint2Y");
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
 
 const interpolations = {
   ease: {
@@ -46,18 +48,49 @@ function initForm() {
   cp2Y.value = interpolations.ease.cp2.y;
 }
 
+// clear the canvas
+function clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function drawLine(
+  finishX,
+  finishY,
+  startX = 0,
+  startY = 0,
+  fillColor = "black"
+) {
+  ctx.strokeStyle = fillColor;
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(finishX, finishY);
+  ctx.stroke();
+  ctx.closePath();
+}
+
 function drawAxes() {
   // Draw x-axis line
-  ctx.beginPath();
-  ctx.moveTo(0, 30);
-  ctx.lineTo(340, 30);
-  ctx.stroke();
-
+  drawLine(340, 30, 0, 30);
   // Draw y-axis line
+  drawLine(30, 340, 30, 0);
+}
+
+function drawPoint(x, y, fillColor = "red") {
+  ctx.fillStyle = fillColor;
   ctx.beginPath();
-  ctx.moveTo(30, 0);
-  ctx.lineTo(30, 340);
-  ctx.stroke();
+  ctx.arc(countPointValue(x), countPointValue(y), 5, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function drawControlLine(controlPoint, extremePoint, fillColor) {
+  drawLine(
+    countPointValue(controlPoint.x),
+    countPointValue(controlPoint.y),
+    countPointValue(extremePoint.x),
+    countPointValue(extremePoint.y),
+    fillColor
+  );
+  drawPoint(controlPoint.x, controlPoint.y, fillColor);
 }
 
 function drawExtremePoints() {
@@ -79,7 +112,6 @@ function drawExtremePoints() {
 
 function drawCurve() {
   const { start, end } = extremePoints;
-  // Cubic Bézier curve
   ctx.beginPath();
   ctx.moveTo(countPointValue(start.x), countPointValue(start.y));
   ctx.bezierCurveTo(
@@ -93,20 +125,43 @@ function drawCurve() {
   ctx.stroke();
 }
 
-// Control points
-// ctx.fillStyle = "red";
-// ctx.beginPath();
-// ctx.arc(cp1.x, cp1.y, 5, 0, 2 * Math.PI); // Control point one
-// ctx.arc(cp2.x, cp2.y, 5, 0, 2 * Math.PI); // Control point two
-// ctx.fill();
+function drawAll() {
+  clear();
+  drawAxes();
+  drawExtremePoints();
+  drawCurve();
+  drawControlLine(
+    { x: cp1X.value, y: cp1Y.value },
+    extremePoints.start,
+    "yellow"
+  );
+  drawControlLine(
+    { x: cp2X.value, y: cp2Y.value },
+    extremePoints.end,
+    "yellow"
+  );
+  // drawPoint(cp1X.value, cp1Y.value, "yellow");
+  // drawPoint(cp2X.value, cp2Y.value, "yellow");
+}
 
 function selectChangeHandler() {
-  console.log(select.value);
+  const { cp1, cp2 } = interpolations[select.value];
+  cp1X.value = cp1.x;
+  cp1Y.value = cp1.y;
+  cp2X.value = cp2.x;
+  cp2Y.value = cp2.y;
+  drawAll();
+}
+
+function inputChangeHandler() {
+  drawAll();
 }
 
 select.addEventListener("change", selectChangeHandler);
+cp1X.addEventListener("change", inputChangeHandler);
+cp1Y.addEventListener("change", inputChangeHandler);
+cp2X.addEventListener("change", inputChangeHandler);
+cp2Y.addEventListener("change", inputChangeHandler);
 
 initForm();
-drawAxes();
-drawExtremePoints();
-drawCurve();
+drawAll();
